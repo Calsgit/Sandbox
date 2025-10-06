e07a1f538845fa49a1533d3adda75c8d0c52a9a7
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-10-07 09:17:36 +1000

    Use EAFP method

--- removenamesfromlist.py
+++ removenamesfromlist.py
@@ -1,10 +1,18 @@
 names = ["Ada", "Alan", "Bill", "John"]
 print(", ".join(names))
 name_to_remove = input("Remove: ")
+# while name_to_remove != "":
+#     if name_to_remove in names:
+#         names.remove(name_to_remove)
+#     else:
+#         print("Name is not in list")
+#     print(", ".join(names))
+#     name_to_remove = input("Remove: ")
+
 while name_to_remove != "":
-    if name_to_remove in names:
+    try:
         names.remove(name_to_remove)
-    else:
+    except ValueError:
         print("Name is not in list")
     print(", ".join(names))
     name_to_remove = input("Remove: ")

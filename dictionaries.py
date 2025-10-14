23ba96b3e124125cc0470fb704c358bff0d9466b
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-10-14 10:07:18 +1000

    Fix variables in function

--- dictionaries.py
+++ dictionaries.py
@@ -2,10 +2,10 @@ names = ['Derek', 'Xavier', 'Bob', 'Chantanelle']
 
 
 def convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(list):
-    dictionary = {}
-    for name in names:
-        dictionary.setdefault(name, len(name))
-    return dictionary
+    string_to_length = {}
+    for string in list:
+        string_to_length.setdefault(string, len(string))
+    return string_to_length
 
 
 name_to_length = convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(names)

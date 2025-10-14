58579f4cf3bfed4337fd1cc993b9dd7a692447ff
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-10-14 10:15:34 +1000

    Add list comprehension alternative

--- dictionaries.py
+++ dictionaries.py
@@ -1,13 +1,17 @@
 names = ['Derek', 'Xavier', 'Bob', 'Chantanelle']
 
 
-def convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(list):
+def convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(strings):
     string_to_length = {}
-    for string in list:
-        string_to_length.setdefault(string, len(string))
+    for string in strings:
+        string_to_length[string] = len(string)
     return string_to_length
 
+def convert_to_dictionary2(strings):
+    return {string: len(string) for string in strings}
 
-name_to_length = convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(names)
 
-print(name_to_length)
+# name_to_length = convert_list_of_strings_to_dictionary_of_strings_and_their_respective_lengths(names)
+# print(name_to_length)
+
+print(convert_to_dictionary2(names))

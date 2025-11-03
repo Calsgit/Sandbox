e4c1aadba5844e87597690281e347f16421e9bcb
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 09:54:49 +1000

    Change name of identical test monitor to ensure equivalence method is not a fluke

--- monitor_test.py
+++ monitor_test.py
@@ -3,12 +3,12 @@ from monitor import Monitor
 
 def main():
     original_test_monitor = Monitor("Tester", 1920, 1080)
-    other_test = Monitor("Other Tester", 200, 400)
-    identical_test_monitor = Monitor("Tester", 1920, 1080)
+    other_test_monitor = Monitor("Other Tester", 200, 400)
+    identical_test_monitor = Monitor("Identical Tester With a Different Name", 1920, 1080)
 
     print(original_test_monitor.get_resolution(), (1920, 1080))
     print(original_test_monitor.get_total_pixels(), 1920 * 1080)
-    print(original_test_monitor == other_test, False)
+    print(original_test_monitor == other_test_monitor, False)
     print(original_test_monitor == identical_test_monitor, True)
 
 

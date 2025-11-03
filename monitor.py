d383c0201f99bb31cf6b01d2d032e1a44dc2da81
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 09:11:50 +1000

    Add monitor file and class

---
+++ monitor.py
@@ -0,0 +1,20 @@
+class Monitor:
+    """Defines a monitor and its width and height in pixels."""
+
+    def __init__(self, model="Monitor", width=0, height=0):
+        """
+        Model: string, name of monitor
+        Width: int, width in pixels
+        Height: int, height in pixels
+        """
+        self.model = model
+        self.width = width
+        self.height = height
+
+    def get_resolution(self):
+        """Return a tuple of monitor width and height."""
+        return (self.width, self.height)
+
+    def get_total_pixels(self):
+        """Calculate how many pixels are on monitor."""
+        return self.width * self.height

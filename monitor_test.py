c0b3d4183fb2d4cc84253dae6cef43976591193f
Calsgit <calam.sinclair@my.jcu.edu.au>
2025-11-04 10:03:38 +1000

    Modify test program to use assert

--- monitor_test.py
+++ monitor_test.py
@@ -1,16 +1,10 @@
 from monitor import Monitor
 
+m1 = Monitor("Tester", 1920, 1080)
+m2 = Monitor("Other Tester", 200, 400)
+m3 = Monitor("Identical Tester With a Different Name", 1920, 1080)
 
-def main():
-    original_test_monitor = Monitor("Tester", 1920, 1080)
-    other_test_monitor = Monitor("Other Tester", 200, 400)
-    identical_test_monitor = Monitor("Identical Tester With a Different Name", 1920, 1080)
-
-    print(original_test_monitor.get_resolution(), (1920, 1080))
-    print(original_test_monitor.get_total_pixels(), 1920 * 1080)
-    print(original_test_monitor == other_test_monitor, False)
-    print(original_test_monitor == identical_test_monitor, True)
-
-
-if __name__ == '__main__':
-    main()
+assert m1.get_resolution() == (1920, 1080), "get_resolution"
+assert m1.get_total_pixels() == 1920 * 1080, "get_total_pixels"
+assert m1 != m2, "__eq__ False"
+assert m1 == m3, "__eq__ True"
